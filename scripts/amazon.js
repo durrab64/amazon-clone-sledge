f5eeff185c05e5ca59109b1c82a78d89251b33ad
durrab64 <mhdurrabrehan@gmail.com>
2026-01-11 19:31:14 +0500

    cart logics implements

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,31 +1,3 @@
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87 
-    },
-    price_cents: 1090,
-},
-{
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4.0,
-        count: 127
-    },
-    price_cents: 2095
-},
-{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    price_cents: 799
-}];
-
 let productHtml = '';
 
 products.forEach((product) => {
@@ -69,12 +41,19 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart">
             Add to Cart
           </button>
         </div>
     `;
 });
 
-console.log(productHtml);
+//console.log(productHtml);
 document.querySelector('.js-product-grid').innerHTML = productHtml;
+
+document.querySelectorAll('.js-add-to-cart')
+.forEach((button)=>{
+    button.addEventListener('click',()=>{
+        console.log('Product added');
+    })
+})
